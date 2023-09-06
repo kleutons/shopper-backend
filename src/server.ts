fbfc0152bbf3dabd827bd08b9a93e147614138a2
kleuton novais <106082564+kleutons@users.noreply.github.com>
2023-09-05 22:35:26 -0300

    fix:  Access-Control Server CROS

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,14 @@ import { productRoutes } from './routes/product.routes';
 
 const app = express();
 
+app.use((req, res, next) => {
+    // Permite acesso de qualquer origem (não seguro somente para produção)
+    res.header('Access-Control-Allow-Origin', '*'); 
+    // Outras configurações CORS podem ser adicionadas aqui, como Access-Control-Allow-Methods, Access-Control-Allow-Headers, etc.
+    next();
+});
+  
+
 app.get('/', (req, res) => {
     res.send("Servidor Iniciado com sucesso!");
 });
@@ -10,4 +18,6 @@ app.get('/', (req, res) => {
 app.use('/product', productRoutes);
 
 
-app.listen(4000);
+app.listen(4000, () => {
+    console.log('Servidor rodando na porta 4000');
+});
